2c21721ab898653a276f6de41918a6c774f84932
Kgermando <katakugermain@gmail.com>
2024-01-05 11:12:33 +0100

    DATABASE_URL

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export default () => ({
     database: {
-      url: isProduction ? process.env.DATABASE_PRIVATE_URL
+      url: isProduction ? process.env.DATABASE_URL
         : `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASS}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`
     },
     token: process.env.JWT_TOKEN,
