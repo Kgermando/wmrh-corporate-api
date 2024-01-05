9b59193e430bd4acc50dcb3ae4db2e50bb0b9dd5
Kgermando <katakugermain@gmail.com>
2024-01-05 12:32:18 +0100

    url

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,10 @@ async function bootstrap() {
       : 'http://localhost:4200',
     credentials: true
   });
+  const HOST = process.env.HOST || 3001;
   const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+    console.log(`App listening on url ${PORT} ${PORT}`);
     console.log('Press Ctrl+C to quit.');
   });
 }
